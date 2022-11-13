3dee91e13ace3940294ca1f950372725901c1195
Andrei <andrei.lauran@astralitech.com>
2022-11-13 16:48:04 +0200

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //not necessary to write api at the beginning, but it's conventional
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products = await _context.Products.ToListAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")] // add http attribute
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+    {
+    }
+    
+    // This allows us to access the products when we use our context and use some of the methods that are defined
+    // inside the DB context that are gonna allow us to query those entities and retrieve the data from db
+    public DbSet<Product> Products { get; set; } // Products will be the name of the table
+}
