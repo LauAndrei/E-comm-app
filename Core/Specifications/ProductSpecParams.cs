d23534e4cf7e333e6a57745649d14588c7725c28
Andrei <andrei.lauran@astralitech.com>
2022-12-17 23:04:35 +0200

    Added UI + made request to get the products

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -15,11 +15,11 @@ public class ProductSpecParams
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
     public string? Sort { get; set; }
-    private string _search;
+    private string? _search;
 
-    public string Search
+    public string? Search
     {
         get => _search;
-        set => _search = value.ToLower();
+        set => _search = value?.ToLower();
     }
 }
